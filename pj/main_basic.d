3ebaa48048d3efc008b556dce8faddfa040a675c
Zeynep Yılmaz <102660467+zenepyilmaz@users.noreply.github.com>
2024-11-30 12:37:06 +0300

    firstcommit

---
+++ pj/main_basic.d
@@ -0,0 +1,3 @@
+./main_basic.o: main_basic.c stm32l476xx.h \
+  D:\Keil_v5\ARM\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\core_cm4.h \
+  D:\Keil_v5\ARM\Packs\Keil\STM32L4xx_DFP\2.7.0\Drivers\CMSIS\Device\ST\STM32L4xx\Include\system_stm32l4xx.h
